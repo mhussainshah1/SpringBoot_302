692fa97e9846e9c3841e03bddb10d80be3a425d7
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-02-26 13:04:07 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/JobRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface JobRepository extends CrudRepository<Job, Long> {
+
+}
---
+++ src/main/java/com/example/demo/SpringBoot302Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot302Application.class, args);
+    }
+
+}
